8ef1fa60a38d9834fb2edcedf60c0c1c20d18edd
Zander <114107976+Femnyx@users.noreply.github.com>
2023-05-03 15:15:32 -0500

    Interfaces

---
+++ Interfaces_Mcvey_Zander/IGenre.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces_Mcvey_Zander
+{
+    interface IGenre
+    {
+
+        public string Esrb { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Title { get; set; }
+
+        public string Describe();
+
+
+
+    }
+}
---
+++ Interfaces_Mcvey_Zander/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces_Mcvey_Zander
+{
+    class Movie : IGenre
+    {
+        public string Esrb { get ; set ; }
+        public string Genre { get ; set ; }
+        public string Title { get ; set ; }
+
+        public Movie(string esrbParam, string genreParam, string titleParam)
+        {
+            Esrb = esrbParam;
+            Genre = genreParam;
+            Title = titleParam;
+        }
+
+        public void PlayMovie()
+        {
+            Console.WriteLine($"{Title} is starting, SSssshhhhhhh!!!");
+        }
+
+        public string Describe()
+        {
+            return $"{Title} is a {Genre} movie, rated {Esrb}";
+        }
+    }
+}
